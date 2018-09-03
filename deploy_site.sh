f4b07c9fc5ff472e089be8311507ca8bb42a8cd1
jfjhh <alex.striff1@gmail.com>
2018-09-03 15:43:18 -0700

    New hosting, new deployment

--- deploy_site.sh
+++ deploy_site.sh
@@ -2,28 +2,11 @@
 
 printf "\033[0;32m[[=== Deploying Site ===]]\033[0;34m\n"
 
-# Load ssh key using keychain.
-printf "\033[0;36m=== Get SSH Key ===\033[0;34m\n"
-[ -z "$HOSTNAME" ] && HOSTNAME=`uname -n`
-source "${HOME}/.keychain/${HOSTNAME}-sh" 2> /dev/null
-
-
-# Only deploy if a SSH key is available.
-if `ssh-add -l 2> /dev/null | tee /dev/stderr | grep -q id_rsa`; then
-	# RSync the files.
-	printf "\033[0;36m=== RSync to Server ===\033[0;34m\n"
-	rsync \
-		-avcz \
-		--delete-delay \
-		-e ssh \
-		html \
-		'bullet@jfjhh.science:/var/www/'
-	# SSH password authentication is disabled on that server, so don't even
-	# think about it.
-else
-	printf "\033[0;31m[[=== No SSH Key Available! ===]]\033[0m\n"
-	exit 1
-fi
+# Make it slightly harder for scrapers to get internal information.
+DEPLOY='c2Z0cCAtbyBVc2VyPXN0cmlmZmEgc2Z0cC5yZWVkLmVkdSA8PDwgJ3B1dCAtcmYgaHRtbCcK'
+DEPLOY_CMD="`base64 -d <<< $DEPLOY`"
+printf "\033[0;35m${DEPLOY_CMD}\033[0;34m\n"
+sh -c "$DEPLOY_CMD"
 
 printf "\033[0;32m[[=== Site Deployed. ===]]\033[0m\n"
 
