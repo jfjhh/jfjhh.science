4a6c8353e49fdf9e2aade81c3db96541403ef505
jfjhh <alex.striff1@gmail.com>
2018-09-04 08:01:22 -0700

    Only upload changed files

--- deploy_site.sh
+++ deploy_site.sh
@@ -3,10 +3,25 @@
 printf "\033[0;32m[[=== Deploying Site ===]]\033[0;34m\n"
 
 # Make it slightly harder for scrapers to get internal information.
-DEPLOY='c2Z0cCAtbyBVc2VyPXN0cmlmZmEgc2Z0cC5yZWVkLmVkdSA8PDwgJ3B1dCAtcmYgaHRtbCcK'
+DEPLOY='c2Z0cCAtb1VzZXI9c3RyaWZmYSBzZnRwLnJlZWQuZWR1Cg=='
 DEPLOY_CMD="`base64 -d <<< $DEPLOY`"
 printf "\033[0;35m${DEPLOY_CMD}\033[0;34m\n"
-sh -c "$DEPLOY_CMD"
+
+MANIFEST="manifest.txt"
+
+(
+rm -f "$MANIFEST";
+echo "get html/$MANIFEST";
+while [ ! -f "$MANIFEST" ]; do
+	sleep 0.5;
+done;
+md5sum -c --quiet "$MANIFEST" 2>/dev/null \
+	| cut --complement -d : -f 2- \
+	| awk '{print "put " $1;}';
+find html -type f -print0 \
+	| xargs -0 md5sum -b > "$MANIFEST";
+echo "put $MANIFEST";
+) | sh -c "$DEPLOY_CMD"
 
 printf "\033[0;32m[[=== Site Deployed. ===]]\033[0m\n"
 
